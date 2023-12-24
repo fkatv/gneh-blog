26e72d4f2c0861f0810aac4da73e1b34af256d4b
fabvictus <f.catalan.v@gmail.com>
2023-12-24 01:09:58 -0300

    mejoras en el espaciado

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
     fontFamily: {
       'roboto': ['roboto', 'arial', 'sans-serif'],
       'lato': ['lato', 'arial', 'sans-serif'],
+    },
+    animation: {
+      'pulse-bg-once': 'pulse-bg-once 4s ease-in infinite', //animation-pulse-bg-once from-red-400 to-yellow-600
+      'wiggle': 'wiggle 1s ease-in-out infinite',
+    },
+    keyframes: {
+      'pulse-bg-once': {
+        '0%': { backgroundColor: 'var(--tw-gradient-from)' },
+        '60%': { backgroundColor: 'var(--tw-gradient-to)' },
+        '100%': { backgroundColor: 'var(--tw-gradient-from)' },
+      },
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-3deg)' },
+        '50%': { transform: 'rotate(3deg)' },
+      }
     }
   },
   darkMode: "class",
