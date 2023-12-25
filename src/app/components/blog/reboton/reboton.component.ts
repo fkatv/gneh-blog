07415cc4d74eaf0264498d40514913d3e033ae4b
fabvictus <f.catalan.v@gmail.com>
2023-12-25 20:55:40 -0300

    avance paso 4 de 7

--- src/app/components/blog/reboton/reboton.component.ts
+++ src/app/components/blog/reboton/reboton.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class RebotonComponent {
   titulo = "Estrategias para crear componentes contextuales de Angular estilizados con Tailwind para mejorar la lectura y reutilización de código"
-
+  funcion = "function useless ( ) => { }";
+  bopen ="{"
+  bclose ="}"
+  f2 = "   if (!options.click) options.click = this.useless();"
   cambiarTitulo(contexto: any) {
     contexto.titulo = "hola!";
   }
