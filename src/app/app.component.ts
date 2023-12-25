9121f8522b5db8beed4a965943a2a8b6911768b5
fabvictus <f.catalan.v@gmail.com>
2023-12-25 13:36:01 -0300

    deploy a firebase hosting +  analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import './firebase/firebaseConfig.js'; // obtener de https://console.firebase.google.com/u/0/project/gneh-fabo
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
